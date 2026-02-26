c88cf312d478a3b972c24acbe7c0963d9d45ceb1
Aravindh-codgod <aravindhr2006@gmail.com>
2026-02-26 15:41:04 +0530

    UC3:Your message here

--- src/PalindromeApp.java
+++ src/PalindromeApp.java
@@ -4,16 +4,25 @@ public class PalindromeApp {
 
         String hardcoded = "madam";
         System.out.println("UC2: Hardcoded check for \"" + hardcoded + "\"");
-        if (isPalindromeHardcoded(hardcoded)) {
-            System.out.println("Result: \"" + hardcoded + "\" is a palindrome.");
-        } else {
-            System.out.println("Result: \"" + hardcoded + "\" is NOT a palindrome.");
-        }
+        System.out.println(isPalindromeHardcoded(hardcoded) ? "Palindrome" : "Not Palindrome");
+
+        String test1 = "racecar";
+        System.out.println("UC3: Reverse string check for \"" + test1 + "\"");
+        System.out.println(isPalindromeByReverse(test1) ? "Palindrome" : "Not Palindrome");
     }
 
     public static boolean isPalindromeHardcoded(String str) {
         return str.equals(new StringBuilder(str).reverse().toString());
     }
+
+    public static boolean isPalindromeByReverse(String str) {
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+        return str.equals(reversed);
+    }
 }
+
 
 
