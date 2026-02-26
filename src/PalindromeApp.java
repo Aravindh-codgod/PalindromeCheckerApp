ab1239590ff90c2126c65507c0babc41117508b4
Aravindh-codgod <aravindhr2006@gmail.com>
2026-02-26 15:44:37 +0530

    UC4:

--- src/PalindromeApp.java
+++ src/PalindromeApp.java
@@ -2,27 +2,24 @@ public class PalindromeApp {
     public static void main(String[] args) {
         System.out.println("Palindrome Checker App - Version 1.0");
 
-        String hardcoded = "madam";
-        System.out.println("UC2: Hardcoded check for \"" + hardcoded + "\"");
-        System.out.println(isPalindromeHardcoded(hardcoded) ? "Palindrome" : "Not Palindrome");
-
-        String test1 = "racecar";
-        System.out.println("UC3: Reverse string check for \"" + test1 + "\"");
-        System.out.println(isPalindromeByReverse(test1) ? "Palindrome" : "Not Palindrome");
+        String test2 = "level";
+        System.out.println("UC4: Char array two-pointer check for \"" + test2 + "\"");
+        System.out.println(isPalindromeCharArray(test2) ? "Palindrome" : "Not Palindrome");
     }
 
-    public static boolean isPalindromeHardcoded(String str) {
-        return str.equals(new StringBuilder(str).reverse().toString());
-    }
-
-    public static boolean isPalindromeByReverse(String str) {
-        String reversed = "";
-        for (int i = str.length() - 1; i >= 0; i--) {
-            reversed += str.charAt(i);
+    public static boolean isPalindromeCharArray(String str) {
+        char[] arr = str.toCharArray();
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            if (arr[left] != arr[right]) return false;
+            left++;
+            right--;
         }
-        return str.equals(reversed);
+        return true;
     }
 }
+
+
 
 
 
